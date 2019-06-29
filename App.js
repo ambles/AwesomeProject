901222e55da293f25563b32fe2eb7c14b1b2260d
Ambles Kwok <ambles.kwok@gmail.com>
2019-06-29 16:15:19 -0400

    Another example of prop!

--- App.js
+++ App.js
@@ -6,19 +6,30 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { AppRegistry, Image } from 'react-native';
+ import React, { Component } from 'react';
+ import { AppRegistry, Text, View } from 'react-native';
 
-export default class Bananas extends Component {
-  render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
-    return (
-      <Image source={pic} style={{width: 193, height: 110}}/>
-    );
-  }
-}
+ class Greeting extends Component {
+   render() {
+     return (
+       <View style={{alignItems: 'center'}}>
+         <Text>Hello {this.props.name}!</Text>
+       </View>
+     );
+   }
+ }
 
-// skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => Bananas);
+ export default class LotsOfGreetings extends Component {
+   render() {
+     return (
+       <View style={{alignItems: 'center', top: 50}}>
+         <Greeting name='Rexxar' />
+         <Greeting name='Jaina' />
+         <Greeting name='Valeera' />
+       </View>
+     );
+   }
+ }
+
+ // skip this line if using Create React Native App
+ AppRegistry.registerComponent('AwesomeProject', () => LotsOfGreetings);
